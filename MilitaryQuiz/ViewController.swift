1950a899d48d708f3d892aa5fddb4dcd74e23f52
Swift-Teacher <brian@swiftteacher.org>
2022-02-15 10:59:06 -0500

    Initial Commit

---
+++ MilitaryQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MilitaryQuiz
+//
+//  Created by Brian Foutty on 2/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
